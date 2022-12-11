03dbf8a0367d0d9533fa2d5c5122ce4c796123d1
sblendorioga <2Ef7qMccswbRgPD>
2022-12-11 12:03:55 +0100

    Refactoring code: renamings and comments

--- ItemsControlWithCheckBox/UserControls/MyItemsControl.xaml.cs
+++ ItemsControlWithCheckBox/UserControls/MyItemsControl.xaml.cs
@@ -35,29 +35,39 @@ namespace ItemsControlWithCheckBox.UserControls
 
         // Using a DependencyProperty as the backing store for People.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty ItemsProperty =
-            DependencyProperty.Register(nameof(Items), typeof(ObservableCollection<ISelectableItem>), typeof(MyItemsControl), new PropertyMetadata(null, OnPropertyChanged));
+            DependencyProperty.Register(nameof(Items), typeof(ObservableCollection<ISelectableItem>), typeof(MyItemsControl), new PropertyMetadata(null, OnItemsPropertyChanged));
 
-        private static void OnPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        
+        private static void OnItemsPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
+            // Called once when the Items property is bound
             var mySelf = (MyItemsControl)d;
 
+            // Add a "SelectionChenged" event handler for each item
             foreach (var item in mySelf.Items)
             {
                 item.SelectionChanged += (s, evt) => CheckSelection(mySelf.Items);
             }
         }
 
+        /// <summary>
+        /// Handle the selection items behavior. No items selected is not permitted.
+        /// </summary>
+        /// <param name="items"></param>
         private static void CheckSelection(ObservableCollection<ISelectableItem> items)
         {
+            // Enable all items
             foreach (var item in items)
             {
                 item.IsEnabled = true;
             }
 
-            var selectionCount = items.Count(p => p.IsSelected);
+            // Gets the count of selected items
+            var selectionCount = items.Count(item => item.IsSelected);
             if(selectionCount == 1)
             {
-                items.Single(p => p.IsSelected).IsEnabled = false;
+                // If a single item is selected becomes reaod only (disabled)
+                items.Single(item => item.IsSelected).IsEnabled = false;
             }
         }
     }
